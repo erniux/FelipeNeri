df84f2f0cf79ff3b04cc41d79eab39c357f0b83a
erniux <erna.tercero@hotmail.com>
2020-08-26 07:29:52 -0500

    storage google

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("activestorage")	
+require ("google/cloud/storage")
  
 
 // Uncomment to copy all static images under ../images to the output folder and reference
